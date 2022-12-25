9e49febce7792be68b77b40c0d1b817567b7fceb
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-12-25 12:15:53 -0500

    Feat: Material UI Container and Box

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,25 @@ import React from "react"
 import Navbar from './Navbar'
 import Section from './Section'
 import About from "./About"
+
+import { 
+  Container,
+  Box 
+} from '@mui/material';
 export default function App() {
 
   return (
     <div>
       <Navbar />
-      <div>
+      <Container className='container' disableGutters sx={{maxWidth: 'xl', height: '100%'}}>
+      <Box sx={{width: '25%', margin: 'auto'}}>
       <Section text='ABOUT'/>
+      </Box>
+      <Box sx={{width: '75%', margin: 'auto'}}>
       <About/>
-      </div>
+      </Box>
       
+      </Container>
     </div>
   )
   
