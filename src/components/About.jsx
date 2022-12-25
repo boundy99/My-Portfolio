8fa4c0569950486a30b1e482d4b4f83d659fc85f
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-12-25 04:32:53 -0500

    Feat: About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function About(props) {
+export default function About() {
   return (
     <div className='about'>
-    <textarea>{props.text}</textarea>
+   
+    <p>
+    I'm a software engineer, and I developed an interest for computer science as a result of being an expert at adjusting the configurations on my home's computers, televisions, and phones.
+    </p>
+    <p>I'm a computer science major presently enrolled at City College of New York. I've been working on algorithms and object oriented programming. I've worked on several solo and group projects, including a Google Cardboard virtual reality project that demonstrates how pointers operate in a linked list and full stack web development that use react and node. js.</p>
+
     </div>
   )
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react"
 import Navbar from './Navbar'
 import Section from './Section'
-
+import About from "./About"
 export default function App() {
 
   return (
     <div>
       <Navbar />
       <Section text='ABOUT'/>
+      <About/>
     </div>
   )
   
