64ecb146c380a3b737fb9342a4dbcb563f6ffa5c
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-12-25 14:48:24 -0500

    Feat: Formatting

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
 export default function Project(props) {
-    return ( <
-        div >
-        <
-        h4 > { props.name } < /h4> <
-        textarea > Description: { props.description } < /textarea> <
-        p > Tools used: < /p>
-
-        <
-        /div>
-    )
+  return (
+    <div>
+    <h4>{props.name}</h4>
+    <textarea>Description: {props.description}</textarea>
+    <p>Tools used: </p>
+    
+    </div>
+  )
 }
