481fc385387ebaa505d166c22d57391c93dff75e
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-12-26 19:38:27 -0500

    Feat: Min and Max width

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Logo from './Logo'
+import React from 'react';
+import Logo from './Logo';
 import { Card, CardContent,  Typography, Container, Box, Grid} from '@mui/material';
 
 export default function ProjectCard(props) {
